4fe4f044b2348d1de587561703ade3f06f879ff5
Raasul62 <rasulaslanov700@gmail.com>
2024-07-31 19:19:03 +0400

    first commit

---
+++ Lesson.dart
@@ -0,0 +1,134 @@
+
+
+void main(){
+  // print("test");
+  // String name="resul";
+  // int yas = 24;
+  // bool isivarmi = true;
+  // print(name);
+  // print (yas);
+  // print(isivarmi);
+  
+  // int a = 25;
+  // int b = 55;
+  // print(a+b);
+  // int c = 55;
+  // int d = 25;
+  // print(c<=d);
+  // var student1 = "resul";
+  // String  student2 ="elvin";
+  // String  student3 = "emil";
+
+  // print(student1 + "   \n" + student2 +" \n "+ student3);
+  // int a = 5;
+  // int b = 7;
+  // int c = 8;
+  // int d = 9;
+  // print( "toplama ${a+b}");
+  // print('cixma ${d-c} ');
+  // print('vurma ${a*d}');
+   
+
+  //  int a = 6;
+  //  if (a<4) {
+  //   print("a boyukdur 4den");
+  //  }
+  //     else {
+  //   print('a kicikdir 4den');
+  //   }
+  //      bool isivarmi = true;
+        
+    
+  //     if (isivarmi == true) {
+  //       print ('emilin isi var');
+
+  //     }
+  //       else {
+  //         print ('emilin isi yoxdur');
+  //       }
+   
+  //  int a = 25;
+  
+  //  if (0<a) {
+  //   print ("a musbet ededdir");
+  //  }
+  //   else {
+  //     print ("a musbet eded deyil");
+    
+   
+   int a = 0;
+      if (0<=a&&a<=12) {
+    print ("0-12 yas usaq");
+   }
+     int h = 13;
+     if (13<=h&&h<=19) {
+      print ("13-19 yas genc");
+     }
+   int c = 20;
+   if (20<=c&&c<=65){
+    print ("20-65 yas yetkin");
+   }
+  int d = 65;
+  if (65<=d&&d<=65) {
+    print ("65 yasdan yuxari yasli");
+  }
+   else ("yasinizi qeyd edin");
+
+   int s = 0;
+   if (0<=s&&s<=0) {
+    print ("0-dan asagi buzlu");
+    }
+
+   
+   int n = 20;
+   if (0<=n&&n<=20) {
+    print (" 0 - 20 arasi soyuq ");
+    }
+
+    int k = 21;
+   if (21<=k&&k<=30) {
+    print (" 21 -30 arasi isdi   ");
+    }
+    
+    int q = 30;
+   if (30<=q&&q<=30) {
+    print (" 30- dan yuxari cox isdi  ");
+    }
+
+   int l = 12345;
+  if (l==12345) {
+ print("düzgün parol");
+}
+ else { 
+ print("yanlış parol");
+}
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+  
+
+
+  
+  
+  
+
+
